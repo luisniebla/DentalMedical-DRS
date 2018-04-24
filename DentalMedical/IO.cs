5650e2c0ed4ec6ff9178a86efd6bb237fd9c8a47
Luis Niebla <luisniebla@protonmail.com>
2018-04-24 11:19:38 -0700

    Successfully exported Excel files to CSV

---
+++ DentalMedical/IO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+
+namespace DentalMedical
+{
+    class IO
+    {
+        ArrayList filesFound = new ArrayList();
+        public string[] SearchDirectory(string dir, string searchCriteria)
+        {
+            string[] files = Directory.GetFiles(dir, "*.xlsx");
+            return files;
+        }
+
+        public void DirSearch(string sDir, string extensionCriteria)
+        {
+
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    
+                    foreach (string f in Directory.GetFiles(d, extensionCriteria))
+                    {
+                        filesFound.Add(f);
+                    }
+                    DirSearch(d, extensionCriteria);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+
+        }
+
+        public ArrayList getFiles(string sDir, string extensionCriteria)
+        {
+            DirSearch( sDir,  extensionCriteria);
+            return filesFound;
+        }
+
+    }
+}
--- DentalMedical/MainWindow.xaml.cs
+++ DentalMedical/MainWindow.xaml.cs
@@ -13,6 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
 namespace DentalMedical
 {
     /// <summary>
@@ -20,9 +26,151 @@ namespace DentalMedical
     /// </summary>
     public partial class MainWindow : Window
     {
+        public object Application { get; private set; }
+        public static Excel.Application xlApp = new Excel.Application();
+        //public static Excel.Workbook xlCBPTemplate = xlApp.Workbooks.Open(@"//serv-az/drs_client_admin/Call Campaigns/ZZ_CBPProccess/CBP Template April (Blackford).xlsx", ReadOnly:true);
+        //Excel.Workbook xlCBPData = xlApp.Workbooks.Open(@"\\serv-az\drs_client_admin\Call Campaigns\ZZ_Merge & CBP RawData\CallBackProof Data\042118\Blackford Lifetime CBP.xlsx");
+
+        //Excel.Worksheet xlFoundAppts = xlCBPTemplate.Worksheets[3];
+        string campaign = "Blackford";
         public MainWindow()
         {
             InitializeComponent();
+            xlApp.DisplayAlerts = false;
+            
+            //ReadExcelHeaders(xlCBPData.Worksheets[4], "CBPData");
+            //xlCBPData.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.UpdateLayout();
+
+            LabelSearchNotification.Content = "Searching...";
+            IO myIO = new IO();
+            string campaign = TextBoxSearchCriteria.Text;
+            string searchCriteria = "*" + campaign + "*Data*.xlsm";
+ 
+            ArrayList foundFiles = myIO.getFiles(@"//serv-az/drs_ofc/05-DENTAL-MEDICAL/", searchCriteria);
+
+            
+            
+            foreach (string file in foundFiles.ToArray())
+            {
+                listBox1.Items.Add(file);
+            }
+
+            listBox1.UpdateLayout();
+
+            LabelSearchNotification.Content = "DONE";
+        }
+
+        private void BtnOpenSelected_Click(object sender, RoutedEventArgs e)
+        {
+            ExcelManipulation();
+        }
+
+        private void ExcelManipulation()
+        {
+            object refMissing = "";
+
+
+
+            // TODO: Programatic finding of CBP using a listview
+            // TODO: Programatic finding of Template using a listview
+            Excel.Workbook xlDataReport;
+            try
+            {
+                xlDataReport = xlApp.Workbooks.Open(listBox1.SelectedItem.ToString(), Password: "LIFE1", ReadOnly: true);
+            }
+            catch (System.NullReferenceException)
+            {
+                xlDataReport = xlApp.Workbooks.Open(listBox1.Items[0].ToString(), Password: "LIFE1", ReadOnly: true);
+            }
+            xlDataReport.Unprotect();
+            ReadExcelHeaders(xlDataReport.Worksheets.get_Item("Master"), "CBPMaster");
+            Excel.Worksheet xlMonth;
+            try
+            {
+                xlMonth = xlDataReport.Worksheets.get_Item("April 2018"); // TODO: Month tab can be named differently
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                xlMonth = xlDataReport.Worksheets.get_Item("Apr 2018");
+            }
+            
+            ReadExcelHeaders(xlMonth, "CBPMonth");
+
+            xlDataReport.Close();
+            //xlCBPTemplate.Close();
+
+            xlApp.Quit();
+
+            MessageBox.Show("DONE");
+
+            /**
+                xlApp.Visible = true;
+
+
+                Excel.Worksheet xlMaster = xlDataReport.Worksheets[1];
+               
+                
+                
+                
+                Excel.Worksheet xlData = xlCBPData.Worksheets[2];
+                int[] dataColumns = { 1, 2, 11 }; // LName, FName, DOB
+                Excel.Range lastCBPLine = xlData.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+                Excel.Range lastReportLine = xlMonth.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+                
+                
+                //Excel.Range lastMasterLine = xlMaster.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+                //Excel.Range range = (Excel.Range)xlMaster.Columns["A", Type.Missing];
+                
+                MessageBox.Show(lastReportLine.Row.ToString());
+                // Process the CBP data by cross-refencing with April tab
+                // FN    LN  DOB  Res  Note  Last Col
+                
+                int[] dataReportColumns = { 1, 2, 7, 9, 12, 19 };
+
+                Excel.Range xlCBPDataRange = xlData.UsedRange;
+
+                Excel.Range xlMonthlyData = xlMonth.UsedRange;
+        **/
+
+        }
+        
+        private ArrayList ReadExcelHeaders(Excel.Worksheet xlwksht, string outputName)
+        {
+            Excel.Range xlCell = xlwksht.Range["A1"];
+            while (xlCell.Value == "" || xlCell.Value2 == null)
+            {
+                xlwksht.Range["A1"].EntireRow.Delete();
+                xlCell = xlwksht.Range["A1"];
+            }
+
+            // FName, LName, BirthDate, Email, HPhone, MPHone, Last Visit, Appt Date, Appt Time
+            ArrayList xl = new ArrayList();
+
+            for(int i = 1; i < 17; i++)
+            {
+                xl.Add(xlwksht.Cells[1, colLoc].Value);
+            }
+            
+            xlwksht.SaveAs(@"C:\Users\data\Desktop\" + outputName + ".csv", Microsoft.Office.Interop.Excel.XlFileFormat.xlCSVWindows, Type.Missing, Type.Missing, true, false, Excel.XlSaveAsAccessMode.xlNoChange, Excel.XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+            return xl;
+        }
+        
+
+        private void BtnImport_Click(object sender, RoutedEventArgs e)
+        {
+            Excel.Application xlApp = new Excel.Application();
+            xlApp.Visible = true;
+            Excel.Workbook xlCBPTemplate = xlApp.Workbooks.Open(@"//serv-az/drs_client_admin/Call Campaigns/ZZ_CBPProccess/CBP Template April (Blackford).xlsx");
+            Excel.Worksheet xlFoundAppts = xlCBPTemplate.Worksheets[3];
+            Excel.Range lastCBPLine = xlFoundAppts.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+
+            // Append found to here.
         }
     }
 }
